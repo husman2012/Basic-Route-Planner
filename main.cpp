9cd4a4ed49934f6193e64abbcdb3ed585904e9b4
husman2012 <37984589+husman2012@users.noreply.github.com>
2019-07-25 15:51:37 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>  // for sort
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -6,46 +7,40 @@
 using std::cout;
 using std::ifstream;
 using std::istringstream;
+using std::sort;
 using std::string;
 using std::vector;
+using std::abs;
 
-enum class State {kEmpty, kObstacle};
+// TODO: Add kStart and kFinish enumerators to the State enum.
+enum class State {kEmpty, kObstacle, kClosed, kPath, kStart, kFinish};
 
-// TODO: Change the return type to use auto or
-// explicitly indicate vector<State>
+// directional deltas
+const int delta[4][2]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+
 vector<State> ParseLine(string line) {
     istringstream sline(line);
     int n;
     char c;
-    // TODO: Change the variable type for row
-    // to be a vector<State>
     vector<State> row;
     while (sline >> n >> c && c == ',') {
-      // TODO: Modify the line below to push_back
-      // a State::kEmpty if n is 0, and push_back
-      // a State::kObstacle otherwise.
-      if(n==0){
+      if (n == 0) {
         row.push_back(State::kEmpty);
-      }else{
+      } else {
         row.push_back(State::kObstacle);
       }
     }
     return row;
 }
 
-// TODO: Modify the return type here as well. Just
-// as above, the board will contain State objects
-// instead of ints.
+
 vector<vector<State>> ReadBoardFile(string path) {
   ifstream myfile (path);
-  // TODO: Modify the board declarationto store 
-  // State objects instead of ints.
   vector<vector<State>> board{};
   if (myfile) {
     string line;
     while (getline(myfile, line)) {
-      // TODO: Modify the row type to use State
-      // objects instead of ints.
       vector<State> row = ParseLine(line);
       board.push_back(row);
     }
@@ -53,27 +48,127 @@ vector<vector<State>> ReadBoardFile(string path) {
   return board;
 }
 
-// TODO: Modify the function signature below to accept
-// cells with State type instead of int type.
+
+/**
+ * Compare the F values of two cells.
+ */
+bool Compare(const vector<int> a, const vector<int> b) {
+  int f1 = a[2] + a[3]; // f1 = g1 + h1
+  int f2 = b[2] + b[3]; // f2 = g2 + h2
+  return f1 > f2; 
+}
+
+
+/**
+ * Sort the two-dimensional vector of ints in descending order.
+ */
+void CellSort(vector<vector<int>> *v) {
+  sort(v->begin(), v->end(), Compare);
+}
+
+
+// Calculate the manhattan distance
+int Heuristic(int x1, int y1, int x2, int y2) {
+  return abs(x2 - x1) + abs(y2 - y1);
+}
+
+
+/** 
+ * Check that a cell is valid: on the grid, not an obstacle, and clear. 
+ */
+bool CheckValidCell(int x, int y, vector<vector<State>> &grid) {
+  bool on_grid_x = (x >= 0 && x < grid.size());
+  bool on_grid_y = (y >= 0 && y < grid[0].size());
+  if (on_grid_x && on_grid_y)
+    return grid[x][y] == State::kEmpty;
+  return false;
+}
+
+
+/** 
+ * Add a node to the open list and mark it as open. 
+ */
+void AddToOpen(int x, int y, int g, int h, vector<vector<int>> &openlist, vector<vector<State>> &grid) {
+  // Add node to open vector, and mark grid cell as closed.
+  openlist.push_back(vector<int>{x, y, g, h});
+  grid[x][y] = State::kClosed;
+}
+
+
+/** 
+ * Expand current nodes's neighbors and add them to the open list.
+ */
+void ExpandNeighbors(const vector<int> &current, int goal[2], vector<vector<int>> &openlist, vector<vector<State>> &grid) {
+  // Get current node's data.
+  int x = current[0];
+  int y = current[1];
+  int g = current[2];
+
+  // Loop through current node's potential neighbors.
+  for (int i = 0; i < 4; i++) {
+    int x2 = x + delta[i][0];
+    int y2 = y + delta[i][1];
+
+    // Check that the potential neighbor's x2 and y2 values are on the grid and not closed.
+    if (CheckValidCell(x2, y2, grid)) {
+      // Increment g value and add neighbor to open list.
+      int g2 = g + 1;
+      int h2 = Heuristic(x2, y2, goal[0], goal[1]);
+      AddToOpen(x2, y2, g2, h2, openlist, grid);
+    }
+  }
+}
+
+
+/** 
+ * Implementation of A* search algorithm
+ */
+vector<vector<State>> Search(vector<vector<State>> grid, int init[2], int goal[2]) {
+  // Create the vector of open nodes.
+  vector<vector<int>> open {};
+  
+  // Initialize the starting node.
+  int x = init[0];
+  int y = init[1];
+  int g = 0;
+  int h = Heuristic(x, y, goal[0],goal[1]);
+  AddToOpen(x, y, g, h, open, grid);
+
+  while (open.size() > 0) {
+    // Get the next node
+    CellSort(&open);
+    auto current = open.back();
+    open.pop_back();
+    x = current[0];
+    y = current[1];
+    grid[x][y] = State::kPath;
+
+    // Check if we're done.
+    if (x == goal[0] && y == goal[1]) {
+      // TODO: Set the init grid cell to kStart, and 
+      // set the goal grid cell to kFinish before returning the grid. 
+      grid[init[0]][init[1]] = State::kStart;
+      grid[goal[0]][goal[1]] = State::kFinish;
+      return grid;
+    }
+    
+    // If we're not done, expand search to current node's neighbors.
+    ExpandNeighbors(current, goal, open, grid);
+  }
+  
+  // We've run out of new nodes to explore and haven't found a path.
+  cout << "No path found!" << "\n";
+  return std::vector<vector<State>>{};
+}
+
+
 string CellString(State cell) {
   switch(cell) {
     case State::kObstacle: return "⛰️   ";
+    case State::kPath: return "🚗   ";
+    case State::kStart: return "🚦   ";
+    case State::kFinish: return "🏁   ";
+    // TODO: Add cases to return "🚦   " for kStart
+    // and "🏁   " for kFinish.
     default: return "0   "; 
   }
-}
-
-void PrintBoard(const vector<vector<State>> board) {
-  for (int i = 0; i < board.size(); i++) {
-    for (int j = 0; j < board[i].size(); j++) {
-      // TODO: Modify the line below to call CellString
-      // on each element of the board before streaming to cout.
-      cout << CellString(board[i][j]);
-    }
-    cout << "\n";
-  }
-}
-
-int main() {
-  auto board = ReadBoardFile("1.board");
-  PrintBoard(board);
-}
